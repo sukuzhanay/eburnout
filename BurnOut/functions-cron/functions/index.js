57f3b2c461833f2562903bc37cbd54c2fbf1de03
joga <jomanetos@gmail.com>
2018-05-22 12:11:00 -0500

    notification para encuesta

--- BurnOut/functions-cron/functions/index.js
+++ BurnOut/functions-cron/functions/index.js
@@ -41,9 +41,33 @@ function diff_dates(dateq){
 
 	return days;
 
+};
+
+
+function to_send(tokens, message){
+
+	if(tokens.length){
+		
+		console.log('FOR THE TOKENS: ',JSON.stringify ( tokens ) );
+		console.log('MESSAGE: ', message );
+
+		const payload = {
+			notification: {
+				title: message.title,
+				body: message.body,
+				sound: "default",
+				click_action: "FCM_PLUGIN_ACTIVITY",
+				  icon: "fcm_push_icon"
+			},
+			data: message
+		};
+		return admin.messaging().sendToDevice(tokens, payload);
+	}
+
 }
 
-function send_notification(email, message){
+
+function tokens_send_notification(email, message){
 
 	let tokens = [];
 
@@ -64,29 +88,14 @@ function send_notification(email, message){
 
 		});
 		
-		if(tokens.length){
-			console.log('SEND NOTIFICATION TO: ',email);
-			console.log('FOR THE TOKENS: ',JSON.stringify ( tokens ) );
-			console.log('MESSAGE: ', message );
-
-			const payload = {
-				notification: {
-					title: message.title,
-					body: message.body,
-					sound: "default",
-					click_action: "FCM_PLUGIN_ACTIVITY",
-				  	icon: "fcm_push_icon"
-				},
-				data: message
-			};
-			return admin.messaging().sendToDevice(tokens, payload);
-		}
+		console.log('SEND NOTIFICATION TO: ',email);
+		return to_send(tokens, message);
 
 	});
 
 };
 
-exports.hourly_job = functions.pubsub.topic('hourly-tick').onPublish((event) => {
+function check_process_survey(){
 
 	refSurvey.orderByChild('id').once('value').then( snapshot => {
 		snapshot.forEach(child => {
@@ -118,7 +127,7 @@ exports.hourly_job = functions.pubsub.topic('hourly-tick').onPublish((event) => 
 
 								if(diffdates==0){
 									const mje = "Te quedan "+diffdates.toString()+" días, valora las recomendaciones que elegistes"
-									send_notification(QAUser.email, { "title":"eBurnOut", "body":mje});
+									tokens_send_notification(QAUser.email, { "title":"eBurnOut", "body":mje});
 								}
 
 							}
@@ -128,7 +137,7 @@ exports.hourly_job = functions.pubsub.topic('hourly-tick').onPublish((event) => 
 					}else{
 						// FALTA QUE REALICE RECOMENDACIONES
 						if(survey.hasOwnProperty('email')){
-							send_notification(survey.email, { "title":"eBurnOut", "body":"Es hora de elejir algunas recomendaciones"});
+							tokens_send_notification(survey.email, { "title":"eBurnOut", "body":"Es hora de elejir algunas recomendaciones"});
 						}
 
 					}
@@ -140,6 +149,62 @@ exports.hourly_job = functions.pubsub.topic('hourly-tick').onPublish((event) => 
 		});
 	});
 
-	return console.log('Verification completed');
+	return console.log('Verification of Survey Process');
+
+};
+
+function check_to_survey(){
+
+
+	refHlogged.orderByChild('email').once('value').then( snapshot => {
+		snapshot.forEach(child => {
+
+			const user = child.val();
+			const key_user = child.key;
+
+			refQAUser.orderByChild('email').equalTo(user.email).limitToLast(1).once('value').then( snapshot => {
+
+				snapshot.forEach(survey => {
+
+					const last_survey = survey.val();
+					const key_last_survey = survey.key;
+
+					if( last_survey.hasOwnProperty('answers') && last_survey.hasOwnProperty('updated_at') ){
+
+						var diffdates = diff_dates(last_survey.updated_at);
+
+						if( diffdates > days_to_answer ){
+
+							var tokens = [];
+							var dias = diffdates - days_to_answer;
+							var mge = "Han pasado "+dias+" día(s) que no realiza alguna encuesta, es hora de realizarla.";
+
+							tokens.push(user.token_message);
+
+							console.log('SEND NOTIFICATION TO: ',user.email);
+							to_send(tokens, { "title":"eBurnOut", "body":mge});
+
+						}
+						
+					}
+
+				});
+
+			});
+
+		});
+
+	});
+
+	return console.log('Verification to Survey');
+
+};
+
+exports.hourly_job = functions.pubsub.topic('hourly-tick').onPublish((event) => {
+
+	check_process_survey();
+	check_to_survey();
+
+	return console.log('Verification Completed');
 
 });
